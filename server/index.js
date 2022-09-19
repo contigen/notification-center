1407a4c456629c44ede8dcebf4fce39eba15dd3e
contigen <69436315+contigen@users.noreply.github.com>
2022-09-19 17:30:11 +0100

    updated code...

--- server/index.js
+++ server/index.js
@@ -9,10 +9,27 @@ const socketIO = require(`socket.io`)(http, {
     origin: CLIENT_ORIGIN,
   },
 });
-
+let posts = [
+  {
+    id: -1,
+    title: `admin's`,
+    postContent: `hello there`,
+    likes: -1,
+  },
+];
 socketIO.on(`connection`, (socket) => {
   writeToConsole(`A user with ${socket.id} ID has connected`);
-  socket.on("disconnect", () => writeToConsole("A user has disconnected"));
+
+  socket.emit(`onLoad`, posts);
+  socket.on(`newPostEvt`, (newPost) => {
+    posts.unshift(newPost);
+    socket.emit(`postsEvt`, posts);
+  });
+  socket.on(`likedPostEvt`, (postID) => {
+    increasePostLike(postID);
+    socket.emit(`postsEvt`, posts);
+  });
+  socket.on("disconnect", () => socket.disconnect());
 });
 App.use(express.urlencoded({ extended: true }));
 App.use(express.json());
@@ -20,10 +37,17 @@ App.use(cors());
 
 App.get(`/api`, (_, res) => res.json({ message: `Hello, I'm on ${PORT}` }));
 
-App.post(`/post`, () => {});
+App.post(`/notify`, async (req, res) => {});
 
 http.listen(PORT, () => writeToConsole(`Server is up and running on ${PORT}`));
 
 function writeToConsole(message = ``) {
   console.log(message);
+}
+function getLikedPost(postID) {
+  return posts.find((post) => post.id === postID);
+}
+function increasePostLike(postID) {
+  const likedPost = getLikedPost(postID);
+  if (likedPost) likedPost.likes++;
 }
