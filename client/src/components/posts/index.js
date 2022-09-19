ecebae7f4a0f5c8d152548e075c48d382bf0fcc3
contigen <69436315+contigen@users.noreply.github.com>
2022-09-19 17:26:57 +0100

    chore: updated code - removed redundant code

--- client/src/components/posts/index.js
+++ client/src/components/posts/index.js
@@ -1,19 +1,25 @@
 import React from "react";
-
-const PostComponent = () => {
-  const POSTS = [
-    { id: 1, title: `new post`, content: `what's up devs` },
-    { id: 2, title: `new post`, content: `what's up devs` },
-  ];
+const PostComponent = ({ socket }) => {
+  const [posts, setPosts] = React.useState(() => {
+    // fetch existing posts if it's available
+    return socket.on(`onLoad`, (post) => setPosts(post)) || [];
+  });
+  React.useEffect(() => {
+    socket.on(`postsEvt`, (post) => setPosts(post));
+  }, [socket]);
+  const sendLike = (id) => socket.emit(`likedPostEvt`, id);
   return (
     <section>
-      <h2 className="center">Recent Posts</h2>
-      {POSTS.map((post) => (
-        <div key={post.id}>
-          <h3>{post.title}</h3>
-          <p>{post.content}</p>
-        </div>
-      ))}
+      <h2 className="center">{!posts.length && `No`} Recent Posts</h2>
+      {!!posts.length &&
+        posts.map((post) => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.postContent}</p>
+            <button onClick={() => sendLike(post.id)}>like post</button>
+            <p>{post.likes}</p>
+          </div>
+        ))}
     </section>
   );
 };
