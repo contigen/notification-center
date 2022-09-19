3123f3ad3106ecbaf23b15b497227b2f7be65dbb
contigen <69436315+contigen@users.noreply.github.com>
2022-09-19 17:29:23 +0100

    fix: fixed text overflowing the page for mobile

--- client/src/pages/page404/index.js
+++ client/src/pages/page404/index.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 const Page404 = () => {
   return (
     <div>
-      <h1 style={{ fontSize: "10rem" }}>404 - Page doesn't exist</h1>
+      <h1 style={{ fontSize: "10rem", wordWrap: "break-word" }}>
+        404 - Page doesn't exist
+      </h1>
       <h2>
         Let's go <Link to="/">home</Link>
       </h2>
