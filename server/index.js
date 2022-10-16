c78ccdb3d1c0bb4d5d7995712d1009baffaf6768
contigen <69436315+contigen@users.noreply.github.com>
2022-10-16 22:10:20 +0100

    chore: Code modification

--- server/index.js
+++ server/index.js
@@ -4,25 +4,44 @@ const PORT = 5000;
 const http = require(`http`).Server(App);
 const cors = require(`cors`);
 const CLIENT_ORIGIN = `http://localhost:3000`;
+const fs = require(`fs`);
 const socketIO = require(`socket.io`)(http, {
   cors: {
     origin: CLIENT_ORIGIN,
   },
 });
-let posts = [
-  {
-    id: -1,
-    title: `admin's`,
-    postContent: `hello there`,
-    likes: -1,
-  },
-];
+
+let posts = readDataFromFile();
+
+function writeDataToFile(postData) {
+  try {
+    const oldUsersData = readDataFromFile();
+    const newUsersData = [postData, ...oldUsersData];
+    // overwrite existing file content
+    fs.appendFileSync(`users.json`, JSON.stringify(newUsersData), {
+      encoding: `utf-8`,
+      flag: `w`,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+function readDataFromFile() {
+  try {
+    const readData = fs.readFileSync(`users.json`);
+    return JSON.parse(readData);
+  } catch (err) {
+    console.error(err);
+  }
+}
 socketIO.on(`connection`, (socket) => {
   writeToConsole(`A user with ${socket.id} ID has connected`);
 
   socket.emit(`onLoad`, posts);
   socket.on(`newPostEvt`, (newPost) => {
     posts.unshift(newPost);
+    writeDataToFile(newPost);
+    posts = readDataFromFile();
     socket.emit(`postsEvt`, posts);
   });
   socket.on(`likedPostEvt`, (postID) => {
@@ -36,8 +55,6 @@ App.use(express.json());
 App.use(cors());
 
 App.get(`/api`, (_, res) => res.json({ message: `Hello, I'm on ${PORT}` }));
-
-App.post(`/notify`, async (req, res) => {});
 
 http.listen(PORT, () => writeToConsole(`Server is up and running on ${PORT}`));
 
